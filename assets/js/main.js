2a97fb69098addd0eb760fc3ddb07355109a32af
annoydey <annoydey@gmail.com>
2022-12-18 09:36:10 -0500

    2nd update

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,50 @@ toggle.onclick = function () {
   main.classList.toggle("active");
 };
 
+
+
+
+document.querySelector(".side-panel-toggle").addEventListener("click", () => {
+  document.querySelector(".topologycon").classList.toggle("side-panel-open");
+    const myStyles = `
+      grid-template-columns: 2fr auto;
+      grid-gap: 20px;
+    `;
+    const element = document.querySelector(".details");
+    element.style.cssText = myStyles;
+  
+});
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+$(".tabs").click(function(){
+    
+    current_fs = $(".active");
+
+    next_fs = $(this).attr('id');
+    next_fs = "#" + next_fs + "1";
+
+    $("fieldset").removeClass("show");
+    $(next_fs).addClass("show");
+
+    current_fs.animate({}, {
+        step: function() {
+            current_fs.css({
+                'display': 'none',
+                'position': 'relative'
+            });
+            next_fs.css({
+                'display': 'block'
+            });
+        }
+    });
+});
+
+});
 // ========================= For Designing (Section) ==================== 
